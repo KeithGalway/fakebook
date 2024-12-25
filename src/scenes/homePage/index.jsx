cb085cc5739673b1d230434e34245254758d8c56
KeithGalway <keith.galway@outlook.com>
2024-12-25 16:55:35 +0800

    add widgets
    create and added UserImage and WidgetWrapper components and started building UserWidget

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,10 +1,20 @@
+import { Box, useMediaQuery } from '@mui/material'
+import { useSelector } from 'react-redux'
 import Navbar from '../navbar/index'
+import UserWidget from 'scenes/widgets/UserWidget'
+
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery('(min-width:1000px')
+    // const { _id, picturePath } = useSelector((state) => state.user) 
+
     return (
-        <div>
+        <Box>
             <Navbar />
-        </div>
+            <Box>
+                <UserWidget />
+            </Box>
+        </Box>
     )
 }
 
